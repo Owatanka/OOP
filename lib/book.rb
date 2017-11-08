71d9dec7732aa94c2f6100550bce414ede8defd4
Dmitriy <30774291+Owatanka@users.noreply.github.com>
2017-11-08 16:57:15 +0200

    update book.rb

--- lib/book.rb
+++ lib/book.rb
@@ -8,12 +8,4 @@ class Book
   def to_s
     "#{title}(#{author})"
   end
-
-  def ==(other)
-    if other.is_a? Book
-      title == other.title && author == other.author
-    else
-      false
-    end
-  end
 end
